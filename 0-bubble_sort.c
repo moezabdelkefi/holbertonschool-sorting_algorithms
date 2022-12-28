ceed6cb5d8097311bd6448ae6b537eb43923a2cf
root <root@DESKTOP-C0MK0L2.localdomain>
2022-12-28 02:14:48 -0930

    fix the function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,9 +8,9 @@
 void bubble_sort(int *array, size_t size)
 {
 	int temp;
-	size_t i, j = 0;
+	size_t i, j;
 
-	for (i = 0; i < size - i - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
@@ -19,6 +19,7 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+
 				print_array(array, size);
 			}
 		}
