dbd1a1eb577597180877123607e1d8069dcd8e64
root <root@DESKTOP-C0MK0L2.localdomain>
2022-12-29 05:12:13 -0930

    adding this function

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,8 +1,41 @@
 #include "sort.h"
 /**
- * 
-*/
+ *
+ */
 void insertion_sort_list(listint_t **list)
 {
-    
+    listint_t *curr = *list;
+    listint_t *prev = NULL;
+    listint_t *next = NULL;
+
+    while (curr != NULL)
+    {
+        next = curr->next;
+        prev = curr->prev;
+
+        while (prev != NULL && prev->n > curr->n)
+        {
+
+            if (prev->prev != NULL)
+            {
+                prev->prev->next = curr;
+            }
+            curr->prev = prev->prev;
+            prev->next = curr->next;
+            if (curr->next != NULL)
+            {
+                curr->next->prev = prev;
+            }
+            curr->next = prev;
+            prev->prev = curr;
+
+            if (curr->prev == NULL)
+            {
+                *list = curr;
+            }
+            prev = curr->prev;
+            print_list(*list);
+        }
+        curr = next;
+    }
 }
