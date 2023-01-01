360224fabc54d32823fbe9ef5f546d2c0d0d0808
root <root@DESKTOP-C0MK0L2.localdomain>
2023-01-01 03:18:24 -0930

    fix file

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,43 @@
 #include "sort.h"
 /**
- *
+ *insertion_sort_list - a function that sorts a doubly linked list of integers
+ *@list: variable of doubly linked list
+ *Return: non
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *curr = *list;
-    listint_t *prev = NULL;
-    listint_t *next = NULL;
+	listint_t *curr = *list;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
-    if (!list)
-    {
-        return;
-    }
+	while (curr != NULL)
+	{
+		next = curr->next;
+		prev = curr->prev;
 
-    while (curr != NULL)
-    {
-        next = curr->next;
-        prev = curr->prev;
+		while (prev != NULL && prev->n > curr->n)
+		{
 
-        while (prev != NULL && prev->n > curr->n)
-        {
+			if (prev->prev != NULL)
+			{
+				prev->prev->next = curr;
+			}
+			curr->prev = prev->prev;
+			prev->next = curr->next;
+			if (curr->next != NULL)
+			{
+				curr->next->prev = prev;
+			}
+			curr->next = prev;
+			prev->prev = curr;
 
-            if (prev->prev != NULL)
-            {
-                prev->prev->next = curr;
-            }
-            curr->prev = prev->prev;
-            prev->next = curr->next;
-            if (curr->next != NULL)
-            {
-                curr->next->prev = prev;
-            }
-            curr->next = prev;
-            prev->prev = curr;
-
-            if (curr->prev == NULL)
-            {
-                *list = curr;
-            }
-            prev = curr->prev;
-            print_list(*list);
-        }
-        curr = next;
-    }
+			if (curr->prev == NULL)
+			{
+				*list = curr;
+			}
+			prev = curr->prev;
+			print_list(*list);
+		}
+		curr = next;
+	}
 }
